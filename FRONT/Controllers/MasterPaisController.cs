72f651f6e0ebe50d6ac219b6b196435646666266
mwpdevelopment <admin@mwpdevelopment.onmicrosoft.com>
2023-01-25 16:20:16 +0100

    Ejemplo FRONT MVC + Bootstrap. Esto usa el CORE como MODELO para tener la INFRASTRUCTURA TRANSVERSAL que piden

---
+++ FRONT/Controllers/MasterPaisController.cs
@@ -0,0 +1,50 @@
+﻿using ECOMMERCE.CORE;
+using FRONT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FRONT.Controllers
+{
+    public class MasterPaisController : Controller
+    {
+        private readonly ILogger<MasterPaisController> _logger;
+
+        public MasterPaisController(ILogger<MasterPaisController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            EntityPaises Paises = new EntityPaises();
+            Paises.list = new List<EntityPais>()
+            {
+                new EntityPais(1,"ESPAÑA"),
+                new EntityPais(2,"FRANCIA"),
+                new EntityPais(3,"SUECIA"),
+                new EntityPais(4,"DINAMARCA"),
+                new EntityPais(5,"MEXICO"),
+                new EntityPais(6,"PORTUGAL"),
+            };
+
+            return View(Paises.list);
+        }
+        public IActionResult Details(int id)
+        {
+
+            return View(new EntityPais(id, "ESPAÑA"));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            // Vista y Modelo de ERROR. No tocar por ahora
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
