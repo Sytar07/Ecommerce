becbeea6df068ff216bad949270f44d5edbdbca2
alberto sierra <asl.ejea@gmail.com>
2023-02-07 18:46:04 +0100

    Varias BD

---
+++ DAL/ImagenesBD.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ECOMMERCE.CORE;
+
+namespace DAL
+{
+    internal class ImagenesBD
+    {
+        public string cadenaConexion_BBDD { get; set; } = "Data Source=localhost\\sqlexpress;Initial Catalog=BASEDATOS;Persist Security Info=False;User ID=USER;Password=CLAVE;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;\"";
+        
+        public EntityImagenes GETALLIMAGENES()
+        {
+            EntityImagenes entityImagenes = new EntityImagenes();
+
+            // declaro la conexion a BBDD
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                // Abro la conexion
+                connection.Open();
+                // Declaro una transaccion
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    // Declaro un COMANDO para ejecutar un PROCEDIMIENTO ALMACENADO
+                    SqlCommand command = new SqlCommand("GETALLIMAGENES", connection);
+                    command.Transaction = sqlTransaction; // Le pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    // parametro SIMPLE
+                    command.Parameters.Add("@action", System.Data.SqlDbType.VarChar).Value = "GET";                    
+                    // EJECUTO EL COMANDO
+                    var reader = command.ExecuteReader();
+                    // LO LEO. 
+                    while (reader.Read())
+                    {
+                        // Añado las Imagenes encontradas a la lista de entidades
+                        entityImagenes.entityImagenes.Add(new EntityImagenes
+                        {
+                            ididentifier_i = (int)reader["ID"],
+                            id_imagen = (int)reader["ID_Imagen"],
+                            path = (string)reader["Path"],
+                            tipo = (string)reader["Tipo"],
+                            owner = (string)reader["Owner"],
+                            fecha_creacion = (DateTime)reader["Fecha_Creacion"],
+                            fecha_modificacion = (DateTime)reader["Fecha_Modificacion"],
+
+                        });
+
+                        Console.WriteLine((int)reader["ID"]);
+                    }
+                    // Cierro el READER
+                    reader.Close();
+
+                  
+                }
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+
+            }
+
+            return entityImagenes;
+        }
+    }
+}
---
+++ DAL/ProductosBD.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ECOMMERCE.CORE;
+
+namespace DAL
+{
+    internal class ProductosBD
+    {
+        public string cadenaConexion_BBDD { get; set; } = "Data Source=localhost\\sqlexpress;Initial Catalog=BASEDATOS;Persist Security Info=False;User ID=USER;Password=CLAVE;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;\"";
+        
+        public EntityProductos GETALLPRODUCTOS()
+        {
+            EntityProductos entityProductos = new EntityProductos();
+
+            // declaro la conexion a BBDD
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                // Abro la conexion
+                connection.Open();
+                // Declaro una transaccion
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    // Declaro un COMANDO para ejecutar un PROCEDIMIENTO ALMACENADO
+                    SqlCommand command = new SqlCommand("GETALLPRODUCTOS", connection);
+                    command.Transaction = sqlTransaction; // Le pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    // parametro SIMPLE
+                    command.Parameters.Add("@action", System.Data.SqlDbType.VarChar).Value = "GET";                    
+                    // EJECUTO EL COMANDO
+                    var reader = command.ExecuteReader();
+                    // LO LEO. 
+                    while (reader.Read())
+                    {
+                        // Añado las Productos encontrados a la lista de entidades
+                        entityProductos.entityProductos.Add(new EntityProductos
+                        {
+                            ididentifier_i = (int)reader["ID"],
+                            id_producto = (int)reader["ID_Imagen"],
+                            nombre = (string)reader["ID_Imagen"],
+                            stock = (int)reader["Owner"],
+                            descripcion = (string)reader["Fecha_Creacion"],
+                            precio = (float)reader["Precio"],
+                            owner = (string)reader["Owner"],
+                            fecha_creacion = (DateTime)reader["Fecha_Creacion"],
+                            fecha_modificacion = (DateTime)reader["Fecha_Modificacion"],
+                            
+
+                        });
+
+                        Console.WriteLine((int)reader["ID"]);
+                    }
+                    // Cierro el READER
+                    reader.Close();
+
+                  
+                }
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+
+            }
+
+            return entityProductos;
+        }
+    }
+}
