764d9a719192257b0160d899954ee81db056fd6a
alberto sierra <asl.ejea@gmail.com>
2023-03-14 18:53:12 +0100

    Delete insert update ProductosDB

--- DAL/ProductosBD.cs
+++ DAL/ProductosBD.cs
@@ -44,16 +44,17 @@ namespace DAL
                         entityProductos.lista.Add(new EntityProducto
                         {
                             ididentifier_i = (int)reader["ID_PRODUCTO"],
-                            nombre = (string)reader["NOMBRE"],
-                            stock = (int)reader["STOCK"],
-                            descripcion = (string)reader["DESCRIPCION"],
-                            precio = (float)reader["PRECIO"],
-                            owner = (string)reader["OWNER"],
-                            fecha_creacion = (DateTime)reader["FECHA_CREACION"],
-                            fecha_modificacion = (DateTime)reader["FECHA_MODIFICACION"],
-                            
+                            nombre_nv = (string)reader["NOMBRE"],
+                            stock_f = (int)reader["STOCK"],
+                            descripcion_nv = (string)reader["DESCRIPCION"],
+                            precio_f = (float)reader["PRECIO"],
+                            Owner_nv = (string)reader["OWNER"],
+                            FechaCreacion_dt = (DateTime)reader["FECHA_CREACION"],
+                            FechaModificacion_dt = (DateTime)reader["FECHA_MODIFICACION"],
 
-                        });
+                                                  
+
+                    });
 
                         Console.WriteLine((int)reader["ID"]);
                     }
@@ -77,6 +78,153 @@ namespace DAL
             }
 
             return entityProductos;
+        }
+
+        public int DELETEPRODUCTO(EntityProducto entityProducto)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_PRODUCTO", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_PRODUCTO", System.Data.SqlDbType.Int).Value = entityProducto.id_producto_nv;
+                    command.Parameters.Add("@NOMBRE", System.Data.SqlDbType.NVarChar).Value = entityProducto.nombre_nv;
+                    command.Parameters.Add("@STOCK", System.Data.SqlDbType.NVarChar).Value = entityProducto.stock_f;
+                    command.Parameters.Add("@DESCRIPCION", System.Data.SqlDbType.Int).Value = entityProducto.descripcion_nv;
+                    command.Parameters.Add("@PRECIO", System.Data.SqlDbType.Int).Value = entityProducto.precio_f;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityProducto.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityProducto.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityProducto.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 1;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
+        }
+  
+        public int INSERTPRODUCTO(EntityProducto entityProducto)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_PRODUCTO", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_PRODUCTO", System.Data.SqlDbType.Int).Value = entityProducto.id_producto_nv;
+                    command.Parameters.Add("@NOMBRE", System.Data.SqlDbType.NVarChar).Value = entityProducto.nombre_nv;
+                    command.Parameters.Add("@STOCK", System.Data.SqlDbType.NVarChar).Value = entityProducto.stock_f;
+                    command.Parameters.Add("@DESCRIPCION", System.Data.SqlDbType.Int).Value = entityProducto.descripcion_nv;
+                    command.Parameters.Add("@PRECIO", System.Data.SqlDbType.Int).Value = entityProducto.precio_f;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityProducto.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityProducto.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityProducto.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 0;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
+        }
+        public int UPDATEPRODUCTO(EntityProducto entityProducto)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_PRODUCTO", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_PRODUCTO", System.Data.SqlDbType.Int).Value = entityProducto.id_producto_nv;
+                    command.Parameters.Add("@NOMBRE", System.Data.SqlDbType.NVarChar).Value = entityProducto.nombre_nv;
+                    command.Parameters.Add("@STOCK", System.Data.SqlDbType.NVarChar).Value = entityProducto.stock_f;
+                    command.Parameters.Add("@DESCRIPCION", System.Data.SqlDbType.Int).Value = entityProducto.descripcion_nv;
+                    command.Parameters.Add("@PRECIO", System.Data.SqlDbType.Int).Value = entityProducto.precio_f;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityProducto.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityProducto.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityProducto.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 0;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
         }
     }
 }
