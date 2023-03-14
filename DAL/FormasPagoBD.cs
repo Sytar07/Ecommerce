f1b1fade51ad71101d0717c6cf7e48f370e38d86
alberto sierra <asl.ejea@gmail.com>
2023-03-14 19:00:30 +0100

    delete insert update FormasPago (tiene errores en los nombres)

--- DAL/FormasPagoBD.cs
+++ DAL/FormasPagoBD.cs
@@ -49,7 +49,9 @@ namespace DAL
                             Owner_nv = (string)reader["OWNER"],
                             FechaCreacion_dt = (DateTime)reader["FECHA_CREACION"],
                             FechaModificacion_dt = (DateTime)reader["FECHA_MODIFICACION"],
-                        });
+
+
+                    });
 
                         Console.WriteLine((int)reader["ID_FPA"]);
                     }
@@ -73,6 +75,148 @@ namespace DAL
             }
 
             return entityFormasPago;
+        }
+
+        public int DELETEFORMASPAGO(EntityFormasPago entityFormasPago)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_FORMASPAGO", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_FPA", System.Data.SqlDbType.Int).Value = entityFormasPago.ididentifier_i;
+                    command.Parameters.Add("@NAME", System.Data.SqlDbType.NVarChar).Value = entityFormasPago.name_nv;
+                    command.Parameters.Add("@TYPE", System.Data.SqlDbType.NVarChar).Value = entityFormasPago.type_nv;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityFormasPago.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityFormasPago.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityFormasPago.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 1;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
+        }
+        public int INSERTFORMASPAGO(EntityFormasPago entityFormasPago)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_FORMASPAGO", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_FPA", System.Data.SqlDbType.Int).Value = entityFormasPago.ididentifier_i;
+                    command.Parameters.Add("@NAME", System.Data.SqlDbType.NVarChar).Value = entityFormasPago.name_nv;
+                    command.Parameters.Add("@TYPE", System.Data.SqlDbType.NVarChar).Value = entityFormasPago.type_nv;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityFormasPago.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityFormasPago.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityFormasPago.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 0;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
+        }
+        public int UPDATEFORMASPAGO(EntityFormasPago entityFormasPago)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_FORMASPAGO", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_FPA", System.Data.SqlDbType.Int).Value = entityFormasPago.ididentifier_i;
+                    command.Parameters.Add("@NAME", System.Data.SqlDbType.NVarChar).Value = entityFormasPago.name_nv;
+                    command.Parameters.Add("@TYPE", System.Data.SqlDbType.NVarChar).Value = entityFormasPago.type_nv;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityFormasPago.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityFormasPago.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityFormasPago.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 0;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
         }
     }
 }
