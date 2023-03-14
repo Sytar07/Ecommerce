4a93dd1292b348e06f42bbee391d539fdf1dc851
alberto sierra <asl.ejea@gmail.com>
2023-03-14 19:11:09 +0100

    delete insert update DireccionesDB (tiene errores en los nombres)

--- DAL/DireccionesBD.cs
+++ DAL/DireccionesBD.cs
@@ -53,6 +53,7 @@ namespace DAL
                             Owner_nv = (string)reader["OWNER"],                            
                             FechaCreacion_dt = (DateTime)reader["FECHA_CREACION"],
                             FechaModificacion_dt = (DateTime)reader["FECHA_MODIFICACION"],
+
                         });
 
                         Console.WriteLine((int)reader["ID"]);
@@ -77,6 +78,158 @@ namespace DAL
             }
 
             return entityDirecciones;
+        }
+
+        public int DELETEDIRECCION(EntityDirecciones entityDirecciones)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_DIRECCION", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_DIRECCION", System.Data.SqlDbType.Int).Value = entityDirecciones.ididentifier_i;
+                    command.Parameters.Add("@DIRECCION", System.Data.SqlDbType.NVarChar).Value = entityDirecciones.direccion_nv;
+                    command.Parameters.Add("@CALLE", System.Data.SqlDbType.NVarChar).Value = entityDirecciones.calle_nv;
+                    command.Parameters.Add("@NUMERO", System.Data.SqlDbType.Int).Value = entityDirecciones.numero_i;
+                    command.Parameters.Add("@PUERTA", System.Data.SqlDbType.Int).Value = entityDirecciones.puerta_nv;
+                    command.Parameters.Add("@CIUDAD", System.Data.SqlDbType.Int).Value = entityDirecciones.ciudad_nv;
+                    command.Parameters.Add("@PAIS", System.Data.SqlDbType.Int).Value = entityDirecciones.pais_nv;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityDirecciones.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityDirecciones.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityDirecciones.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 1;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
+        }
+        public int INSERTDIRECCION(EntityDirecciones entityDirecciones)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_DIRECCION", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_DIRECCION", System.Data.SqlDbType.Int).Value = entityDirecciones.ididentifier_i;
+                    command.Parameters.Add("@DIRECCION", System.Data.SqlDbType.NVarChar).Value = entityDirecciones.direccion_nv;
+                    command.Parameters.Add("@CALLE", System.Data.SqlDbType.NVarChar).Value = entityDirecciones.calle_nv;
+                    command.Parameters.Add("@NUMERO", System.Data.SqlDbType.Int).Value = entityDirecciones.numero_i;
+                    command.Parameters.Add("@PUERTA", System.Data.SqlDbType.Int).Value = entityDirecciones.puerta_nv;
+                    command.Parameters.Add("@CIUDAD", System.Data.SqlDbType.Int).Value = entityDirecciones.ciudad_nv;
+                    command.Parameters.Add("@PAIS", System.Data.SqlDbType.Int).Value = entityDirecciones.pais_nv;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityDirecciones.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityDirecciones.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityDirecciones.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 0;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
+        }
+        public int UPDATEDIRECCION(EntityDirecciones entityDirecciones)
+        {
+
+            int salida = 0;
+            using (SqlConnection connection = new SqlConnection(cadenaConexion_BBDD))
+            {
+                connection.Open();
+                SqlTransaction sqlTransaction = connection.BeginTransaction();
+                try
+                {
+                    SqlCommand command = new SqlCommand("CUD_DIRECCION", connection);
+                    command.Transaction = sqlTransaction; // LE pasamos la transaccion
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.Add("@ID_DIRECCION", System.Data.SqlDbType.Int).Value = entityDirecciones.ididentifier_i;
+                    command.Parameters.Add("@DIRECCION", System.Data.SqlDbType.NVarChar).Value = entityDirecciones.direccion_nv;
+                    command.Parameters.Add("@CALLE", System.Data.SqlDbType.NVarChar).Value = entityDirecciones.calle_nv;
+                    command.Parameters.Add("@NUMERO", System.Data.SqlDbType.Int).Value = entityDirecciones.numero_i;
+                    command.Parameters.Add("@PUERTA", System.Data.SqlDbType.Int).Value = entityDirecciones.puerta_nv;
+                    command.Parameters.Add("@CIUDAD", System.Data.SqlDbType.Int).Value = entityDirecciones.ciudad_nv;
+                    command.Parameters.Add("@PAIS", System.Data.SqlDbType.Int).Value = entityDirecciones.pais_nv;
+                    command.Parameters.Add("@OWNER", System.Data.SqlDbType.Int).Value = entityDirecciones.Owner_nv;
+                    command.Parameters.Add("@FECHA_CREACION", System.Data.SqlDbType.Int).Value = entityDirecciones.FechaCreacion_dt;
+                    command.Parameters.Add("@FECHA_MODIFICACION", System.Data.SqlDbType.Int).Value = entityDirecciones.FechaModificacion_dt;
+
+                    command.Parameters.Add("@delete", System.Data.SqlDbType.SmallInt).Value = 0;
+
+                    command.Parameters.Add("@ID_RETURN", System.Data.SqlDbType.Int).Value = 0;
+                    command.Parameters["@ID_RETURN"].Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    salida = int.Parse(command.Parameters["@ID_RETURN"].Value.ToString());
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Ha fallado ROLLBACK a la transaccion
+                    sqlTransaction.Rollback();
+                    throw new Exception(ex.Message);
+                    return -1;
+                }
+                finally
+                {
+                    // SI funciona la transaccion le damos adelante. COMMIT
+                    sqlTransaction.Commit();
+                }
+            }
+
+            return salida;
         }
     }
 }
